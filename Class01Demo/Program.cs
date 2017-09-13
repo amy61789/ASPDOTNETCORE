ae7e7ca427ee6c30b7cd424b991c6c52b7c2a445
Amy Funk <amy61789@outlook.com>
2017-09-13 11:48:25 -0700

    Add files via upload

---
+++ Class01Demo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Class01Demo
+{
+    class Program
+    {
+        //string x = "seventy-two";
+
+        static void Main(string[] args)
+        {
+            string myMainVariable;
+            myMainVariable = "This is my variable";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(myMainVariable);
+            SayHello("Hello!");
+            SayHello(72);
+            //string words = Console.ReadLine();
+            //Console.WriteLine(words);
+            IterationStatements();
+            SelectiveStatements();
+            Console.Read();
+            
+        }
+
+        static void SayHello(string greeting)
+        {
+            Console.WriteLine(string.Format("This is my greeting {0}",greeting));
+            Console.WriteLine($"This is the new standard {greeting}");
+        }
+
+        static void SayHello(int greeting)
+        {
+            Console.WriteLine(string.Format("This is my greeting {0}", greeting));
+            Console.WriteLine($"This is the new standard {greeting}");
+        }
+
+        static void IterationStatements()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] numberArray = new int[] { 72, 42, 21, 38 };
+            foreach(int number in numberArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            int x = 0;
+            while(x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+            int n = 0;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            while(n < 5);
+        }
+        static void SelectiveStatements()
+        {
+            Console.WriteLine("What is your name?");
+            string userInput = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(userInput.Trim()))
+            {
+                Console.WriteLine("Please type a name!");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome {userInput}");
+            }
+        }
+    }
+}
---
+++ Class02Demo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Class02Demo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double balance = 5820;
+            Console.WriteLine("ATM");
+            InputHandler(balance);
+        }
+
+        static public int CashCash()
+        {
+            try
+            {
+                Console.WriteLine("What would you like to do? Enter a number: ");
+                Console.WriteLine("1. View your balance ");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Deposit");
+                int input = Convert.ToInt32(Console.ReadLine());
+                return input;
+            }
+            catch(FormatException fe)
+            {
+                Console.WriteLine("Put in a number");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Yay task complete!");
+            }
+
+            return 0;
+           
+        }
+
+        static public double ViewBal(double balance)
+        {
+            Console.WriteLine($"Your balance is {balance}");
+            InputHandler(balance);
+            return balance;
+        }
+
+        static public void InputHandler(double balance)
+        {
+
+            int selection = CashCash();
+
+            switch (selection)
+            {
+                case 1:
+                    ViewBal(balance);
+                    break;
+                case 2:
+                    Withdraw(balance);
+                    break;
+                case 3:
+                    Deposit(balance);
+                    break;
+                default:
+                    Console.WriteLine("Please choose from available options.");
+                    InputHandler(balance);
+                    break;
+            }
+        }
+
+        static public void Withdraw(double balance)
+        {
+            Console.WriteLine("How much would you like to withdraw today?");
+            int withdrawAmount = Convert.ToInt32(Console.ReadLine());
+            balance -= withdrawAmount;
+            //return (int)balance;
+            ViewBal(balance);
+            InputHandler(balance);
+        }
+
+        static public void Deposit(double balance)
+        {
+            Console.WriteLine("How much money would you like to deposit?");
+            int depositAmount = Convert.ToInt32(Console.ReadLine());
+            balance += depositAmount;
+            ViewBal(balance);
+            InputHandler(balance);
+        }
+    }
+}
