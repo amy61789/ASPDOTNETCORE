39c4f75d3d798938e8c762920aa6485797b7a623
Amy Funk <amy61789@outlook.com>
2017-09-26 14:08:18 -0700

    Creating controllers in .Net Core!

---
+++ Class12Demo/Class12Demo/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class12Demo.Controllers
+{
+    public class HomeController
+    {
+        public string Index()
+        {
+            return "Welcome to our Movie Library!";
+        }
+    }
+}
