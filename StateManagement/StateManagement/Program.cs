57921be928177ad6164dc7dab89e5bb4a972474f
Amy Funk <amy61789@outlook.com>
2017-09-26 13:52:44 -0700

    Stacks and Queues!

---
+++ StateManagement/StateManagement/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StateManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Cat josie = new Cat();
+
+            Cat belle = josie;
+            GC.Collect();
+        }
+    }
+}
