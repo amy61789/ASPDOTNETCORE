9d02ba57889e922e72eb87b84b21b00530807cce
Amy Funk <amy61789@outlook.com>
2017-09-29 15:34:09 -0700

    Add files via upload

---
+++ Class13Demo/Class13Demo/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class13Demo.Controllers
+{
+    public class ErrorController
+    {
+        [Route("Error")]
+        public string Index()
+        {
+            return "Something went wrong";
+        }
+    }
+}
