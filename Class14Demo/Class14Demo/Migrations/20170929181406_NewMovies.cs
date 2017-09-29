a8c1bae8fbcc3be4b783446df6e8bf0372932065
Amy Funk <amy61789@outlook.com>
2017-09-29 15:30:58 -0700

    Add files via upload

---
+++ Class14Demo/Class14Demo/Migrations/20170929181406_NewMovies.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Class14Demo.Migrations
+{
+    public partial class NewMovies : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Class15Demo/Class15Demo/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Class15Demo.Models;
+
+namespace Class15Demo.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Range(30,400)]
+        public int RunTime { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Display(Name = "Starring")]
+        [Required]
+        public string LeadStar { get; set; }
+    }
+}
